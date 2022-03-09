791421b89b5d3c4c73520770e962d1cc571cdf8e
Vitalijs Zapans <s9_zapans_v@venta.lv>
2022-03-09 16:51:37 +0200

    Edit: add get functions in the Proffesor

--- src/models/Professor.java
+++ src/models/Professor.java
@@ -1,9 +1,20 @@
 package models;
 
 public class Professor {
-	//mainigie
+	//1. mainigie
 	private String name;
 	private String surname;
 	private ProfDegree degree;
+	
+	// 2. get funkcijas	
+	public String getName() {
+		return name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public ProfDegree getDegree() {
+		return degree;
+	}
 	
 }
