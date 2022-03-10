88d21bea8b2d6030e05c62d13f54204018180d85
Vitalijs Zapans <s9_zapans_v@venta.lv>
2022-03-10 11:47:52 +0200

    Add: Set, construction, toString function for Grade

--- src/models/Grade.java
+++ src/models/Grade.java
@@ -13,21 +13,63 @@ public class Grade {
 	public int getId() {
 		return id;
 	}
-
 	public int getValue() {
 		return value;
 	}
-
 	public Student getStudent() {
 		return student;
 	}
-
 	public Course getCourse() {
 		return course;
 	}
 	
+	// 2.2. set funkcija
+	public void setId() {
+		this.id = idCounter++;
+	}
+	public void setValue(int value) {
+		if (value >= 0 && value <= 10) {
+			this.value = value;
+		}
+		else {
+			this.value = 0;
+		}
+	}	
+	public void setStudent(Student student) {
+		if (student != null) {
+			this.student = student;
+		}
+		else {
+			this.student = new Student();
+		}
+	}
+	public void setCourse(Course course) {
+		if (course != null) {
+			this.course = course;
+		}
+		else {
+			this.course = new Course();
+		}
+	}
 	
+	//3. konstruktori
+	public Grade() {
+		setId();
+		setValue(0);
+		setStudent(new Student());
+		setCourse(new Course());
+	}
+	public Grade(int value, Student student, Course course) {
+		setId();
+		setValue(value);
+		setStudent(student);
+		setCourse(course);
+	}
 	
+	// 4. toString funkcija
+	@Override
+	public String toString() {
+		return "Grade [id=" + id + ", value=" + value + ", student=" + student + ", course=" + course + "]";
+	}
 	
-
 }
