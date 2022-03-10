910aa6259aba65cc2916779750e4e2409d8262b8
Vitalijs Zapans <s9_zapans_v@venta.lv>
2022-03-10 11:31:26 +0200

    Add: class with all for Student

---
+++ src/models/Student.java
@@ -0,0 +1,59 @@
+package models;
+
+public class Student {
+	//1. mainigie
+	private int id;
+	private String name;
+	private String surname;
+
+	private static int idCounter = 10000;
+
+	//2.1 get funkcijas
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	
+	//2.2. set funkcijas
+	public void setId() {
+		this.id = idCounter++;
+	}
+	public void setName(String name) {
+		if (name != null && name.matches("[A-ZĀĒŪĪĻĶĢŠŽČŅ]{1}[a-zēūīāšģķļņčž]+\s?([A-ZĀĒŪĪĻĶĢŠŽČŅ]{1}[a-zēūīāšģķļņčž]+)?")){				// https://regex101.com/
+			this.name = name;
+		}
+		else {
+			this.name = "notknown";
+		}
+	}	
+	public void setSurname(String surname) {
+		if (surname != null && surname.matches("[A-ZĀĒŪĪĻĶĢŠŽČŅ]{1}[a-zēūīāšģķļņčž]+\s?([A-ZĀĒŪĪĻĶĢŠŽČŅ]{1}[a-zēūīāšģķļņčž]+)?")){				// https://regex101.com/
+			this.surname = surname;
+		}
+		else {
+			this.surname = "notknown";
+		}
+	}
+	
+	// 3. konstruktori
+	public Student () {
+		setName("Test");
+		setSurname("Student");
+		setId();
+	}
+	public Student (String name, String surname){
+		setName(name);
+		setSurname(surname);
+		setId();
+	}
+	
+	// 4. toString funkcijas
+	public String toString() {
+		return id + " " + name + " " + surname;
+	}
+}
