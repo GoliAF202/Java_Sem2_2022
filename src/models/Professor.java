059bfe727104688e1dafe34eeaa9e0bb88e6e814
Vitalijs Zapans <s9_zapans_v@venta.lv>
2022-03-10 09:42:11 +0200

    Edit: add set functions (surname and degree) in the Proffesor

--- src/models/Professor.java
+++ src/models/Professor.java
@@ -19,12 +19,30 @@ public class Professor {
 
 	// 3. set funkcijas
 	public void setName(String name) {
-		if (name.matches("[A-Z]{1}[a-z]+")){				// https://regex101.com/
+		if (name != null && name.matches("[A-ZĀĒŪĪĻĶĢŠŽČŅ]{1}[a-zēūīāšģķļņčž]+\s?([A-ZĀĒŪĪĻĶĢŠŽČŅ]{1}[a-zēūīāšģķļņčž]+)?")){				// https://regex101.com/
 			this.name = name;
 		}
 		else {
 			this.name = "notknown";
 		}
 	}	
+	public void setSurname(String surname) {
+		if (surname != null && surname.matches("[A-ZĀĒŪĪĻĶĢŠŽČŅ]{1}[a-zēūīāšģķļņčž]+\s?([A-ZĀĒŪĪĻĶĢŠŽČŅ]{1}[a-zēūīāšģķļņčž]+)?")){				// https://regex101.com/
+			this.surname = surname;
+		}
+		else {
+			this.surname = "notknown";
+		}
+	}
+	public void setDegree(ProfDegree degree) {
+		if (degree != null) {
+			this.degree = degree;
+		}
+		else {
+			this.degree = ProfDegree.master;
+		}
+	}		
+	
+	
 	
 }
